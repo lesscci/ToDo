a77359ee4a02d97d8bfe53b56893f8b541c575a4
@delafuentea2 <a.delafuente1dawnuria2021@gmail.com>
2023-05-05 22:38:23 +0200

    Adrian::Controllers

---
+++ database/migrations/005_create_tasks_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('tarea');
+            $table->unsignedBigInteger('state_id')->foreignId('state_id');
+            $table->unsignedBigInteger('user_id')->foreignId('user_id');
+            $table->timestamps();
+
+            $table->foreign('state_id')->references('id')->on('states')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
